fff80feb7045b83d66fb3ffa7206da99f511836a
hussain2023 <iamhussainali2@gmail.com>
2022-12-24 18:26:08 +0530

    added for and while loop

---
+++ js/fourForLoop.js
@@ -0,0 +1,13 @@
+console.log("For Loop");
+// print 0-10
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+let total = 0;
+for (let i = 0; i <= 10; i++) {
+  total = total + i;
+  //   console.log(total);
+}
+console.log(total);
---
+++ js/threeWhileLoop.js
@@ -0,0 +1,19 @@
+console.log("while loop");
+// print 0-100
+let i = 0;
+while (i <= 100) {
+  // console.log(i);
+  i++;
+}
+
+let total = 0;
+let m = 0;
+while (m <= 100) {
+  total = total + m;
+  m++;
+}
+// console.log(total);
+
+let num = 100;
+let total1 = (num * (num + 1)) / 2;
+console.log(total1);
