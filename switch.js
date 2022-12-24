aaf5f07d10e1a1bdca7ace8586ce9edcb5c7b952
hussain2023 <iamhussainali2@gmail.com>
2022-12-24 17:55:56 +0530

    switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+let day = -1;
+switch (day) {
+  case 0:
+    console.log("sunday");
+    break;
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  default:
+    console.log("invalid day");
+}
